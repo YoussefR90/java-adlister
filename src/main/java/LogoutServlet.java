5264bf741ff687e09455f4a5f0d0f847e70dae54
Youssef Rivas <Youssef.Rivas@gmail.com>
2021-09-10 10:28:13 -0500

    "added adlister to git"

---
+++ src/main/java/LogoutServlet.java
@@ -0,0 +1,26 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "LogoutServlet", value = "/logout")
+public class LogoutServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        //todo lecture
+//        HttpSession currentSession = request.getSession();
+//        currentSession.invalidate();// logs out of current session
+//        response.sendRedirect("/login");// redirects to the login page
+//    }
+        //todo exercise walk through
+        request.getSession().invalidate();
+        response.sendRedirect("/login");
+
+//        @Override
+//        protected void doPost (HttpServletRequest request, HttpServletResponse response) throws
+//        ServletException, IOException {
+//
+//        }
+    }
+}
